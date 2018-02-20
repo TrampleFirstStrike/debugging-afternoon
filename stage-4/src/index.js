351dc183ea856a4623c60d3a12788253446f272d
Jordan Oakes <jordanoakes@Jordans-MacBook-Air.local>
2018-02-20 16:04:35 -0600

    this was tough, guys

--- stage-4/src/index.js
+++ stage-4/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
+import { Provider, promiseMiddleware } from 'react-redux';
 import store from './redux/store';
+import redux from 'react-redux';
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
 
     <Provider store={store}>
+        <HashRouter>
         <App />
+        </HashRouter>
     </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
